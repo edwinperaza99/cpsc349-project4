0e2aa1752f95bc0fec4b7ac642d31f8453571cbe
FrozenTrojan <ethanton@csu.fullerton.edu>
2022-12-16 22:50:20 -0800

    Fixed Input for login and sign up

--- src/app.jsx
+++ src/app.jsx
@@ -425,16 +425,35 @@ function LogIn() {
 									required=""
 								/>
 							</div>
-							<div className="mt-4">
-								<label className="block">Password</label>
-								<input
+							<div class="flex justify-center">
+								<div class="mb-3 xl:w-96">
+									<label for="examplePassword0" class="form-label inline-block mb-2 text-gray-700"
+									>Password input</label
+									>
+									<input
 									type="password"
-									name="password"
-									placeholder="Password"
-									className="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600"
-									required=""
-								/>
-							</div>
+									class="
+										form-control
+										block
+										w-full
+										px-3
+										py-1.5
+										text-base
+										font-normal
+										text-gray-700
+										bg-white bg-clip-padding
+										border border-solid border-gray-300
+										rounded
+										transition
+										ease-in-out
+										m-0
+										focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none
+									"
+									id="examplePassword0"
+									placeholder="Password input"
+									/>
+								</div>
+								</div>
 							<div className="flex items-baseline justify-between">
 								<button
 									type="submit"
